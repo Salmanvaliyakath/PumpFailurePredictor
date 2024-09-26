9d0ddb1043241f86a1040c906324ab4392e578a2
Salman Valiyakath <49808077+Salmanvaliyakath@users.noreply.github.com>
2024-09-27 02:29:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-FILE_PATH = os.getenv('FILE_PATH')
+# FILE_PATH = os.getenv('FILE_PATH')
 MODEL_PATH = os.getenv('MODEL_PATH')
 FEATURES = ['vibration_level', 'temperature_C', 'pressure_PSI', 'flow_rate_m3h']
 TARGET = 'failure'
@@ -29,6 +29,14 @@ def load_data(file_path):
     return pd.read_csv(file_path)
 
 def preprocess_data(df):
+    """Preprocess data to create sequences for model input.
+    
+    Args:
+        df: The input DataFrame containing features with ou without target.
+
+    Returns:
+        np.array: Arrays of input sequences and corresponding target values.
+    """
     X = df[FEATURES]
     if TARGET in df.columns:
         Y = df[TARGET]
@@ -45,24 +53,58 @@ def preprocess_data(df):
     else:
         return np.array(X_local), None
 
-def save_predictions(df, predictions):
+def save_predictions(df, predictions, FILEPATH):
     df['predict'] = None  # Initialize 'predict' column
     df['predict'].iloc[SEQUENCE_LENGTH:] = predictions.flatten()  # Assign predictions
-    df.to_csv(f'{FILE_PATH}_predicted.csv', index=False)
+    df.to_csv(FILEPATH.replace('.csv','_predicted.csv'), index=False)
+    
     
 def scale_features(df):
+    """Scale features using MinMaxScaler to normalize data.
+    
+    Args:
+        df: The input DataFrame to scale features.
+
+    Task:
+        The scaler object fitted to the features.
+    """
     scaler = MinMaxScaler()
     df[FEATURES] = scaler.fit_transform(df[FEATURES])
-    return scaler
 
 def load_trained_model(model_path):
+    """Load a trained Keras model.
+    
+    Args:
+        model_path (str): Path to the trained model file.
+
+    Returns:
+        The loaded Keras model.
+    """
     return load_model(model_path)
 
 def predict(model, X_local):
+    """Make predictions using the trained model.
+    
+    Args:
+        model (keras.Model): The trained model.
+        X_local (np.array): Input data for prediction.
+
+    Returns:
+        np.array: Array of predicted classes.
+    """
     predictions = model.predict(X_local)
     return np.where(predictions > 0.5, 1, 0)
 
 def evaluate_model(y_true, y_pred):
+    """Evaluate the model and print the classification report only when the target variable is available.
+    
+    Args:
+        y_true (np.array): True class labels.
+        y_pred (np.array): Predicted class labels.
+
+    Returns:
+        str: The classification report as a string.
+    """
     report = classification_report(y_true, y_pred, target_names=['no failure', 'failure'])
     return report
 
@@ -75,18 +117,19 @@ def startup_event():
 @app.post("/predict/")
 def predict_failure(request: Filepath):
     """Endpoint to predict failure based on input data."""
-    df = load_data(FILE_PATH)
+    FILEPATH = request.filepath
+    df = load_data(FILEPATH)
     scale_features(df)
-    input_data = df.copy()
-    X_local, _ = preprocess_data(input_data)
+    X_local, _ = preprocess_data(df)
     y_test_pred = predict(model, X_local)
-    save_predictions(df, y_test_pred)
+    save_predictions(df, y_test_pred, FILEPATH)
     
     if TARGET in df.columns:
         report = evaluate_model(df[TARGET][SEQUENCE_LENGTH:].values, y_test_pred)
         return {"report": report}
     
-    return {f"prediction saved :{FILE_PATH.replace('.csv','_predicted.csv')}"}
+    else:
+        return {f"prediction saved :{request.filepath.replace('.csv','_predicted.csv')}"}
 
 if __name__ == "__main__":
     import uvicorn
